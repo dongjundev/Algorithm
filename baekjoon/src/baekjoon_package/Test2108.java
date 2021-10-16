9898c593e8e8998fe53fb1dc0690dc0d02be29ac
dongjun <yh02195@gmail.com>
2021-10-17 00:11:35 +0900

    정렬

--- baekjoon/src/baekjoon_package/Test2108.java
+++ baekjoon/src/baekjoon_package/Test2108.java
@@ -1,31 +1,45 @@
 package baekjoon_package;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
+
 
 public class Test2108 {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        int a = 0;
-        int b = 0;
-        int c = 0;
-        int d = 0;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        List<Integer> list =new ArrayList<>();
+        int [] arr =new int[n];
+        int [] check =new int[8001];
+        int max=0;
+        int sum=0;
+        for(int i=0;i<n;i++) {
+            arr[i]=Integer.parseInt(br.readLine());
+            sum+=arr[i];
+            check[arr[i]+4000]++;
+        }
 
-        for(int i=0; i<N; i++)
-            arr[i] = Integer.parseInt(br.readLine());
-
+        System.out.println((int)Math.round((double)sum/n)); //산술평균
         Arrays.sort(arr);
+        System.out.println(arr[n/2]); //중앙값
+        for(int i=0;i<8001;i++) {
+            if(check[i]>=max) {
+                max=check[i];
+            }
+        }
+        for(int i=0;i<8001;i++) {
+            if(check[i]==max) {
+                list.add(i-4000);
+            }
+        }
+        if(list.size()==1) {
+            System.out.println(list.get(0));
+        }
+        else {
+            Collections.sort(list);
+            System.out.println(list.get(1));
+        }
+        System.out.println(Math.abs(arr[arr.length-1]-arr[0]));  //범위
 
-        for(int one : arr)
-            a+=one;
-
-        a = a/N;
-        b = arr[N/2+1];
-        c = 0;
-        d = arr[N-1]-arr[0];
     }
 }
