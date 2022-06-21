1e06bf1a48531cc946b90ec49c7e3edb444a2bc7
dongjun <yh02195@gmail.com>
2022-06-21 22:19:19 +0900

    leet 1642

---
+++ LeetCode/src/Test1642.java
@@ -0,0 +1,38 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Test1642 {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+
+        int maxIdx = 0;
+
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2-o1;
+            }
+        });
+
+        for (int i = 1; i < heights.length && bricks >= 0; i++) {   //bricks 갯수 주의!!
+            if (heights[i] <= heights[i - 1]) {
+                maxIdx = i;
+            } else {
+                int diff = heights[i] - heights[i - 1];
+                bricks -= diff;
+                q.offer(diff);
+
+                while (bricks < 0 && ladders > 0 && !q.isEmpty()) {     //bricks 갯수 주의!!
+                    int poll = q.poll();
+                    bricks += poll;
+                    ladders--;
+                }
+
+                if (bricks >= 0) {
+                    maxIdx = i;
+                }
+            }
+        }
+
+        return maxIdx;
+    }
+}
--- LeetCode/src/Test820.java
+++ LeetCode/src/Test820.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Test820 {
-    public int minimumLengthEncoding(String[] words) {
+    public static int minimumLengthEncoding(String[] words) {
         Set<String> set = new HashSet<>(Arrays.asList(words));
         for(String word: words) {
             for(int i = 1; i < word.length(); i++)
