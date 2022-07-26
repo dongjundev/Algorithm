8dc35e584be6aa388ac10c037960c83302c76e4c
dongjun <yh02195@gmail.com>
2022-07-26 23:21:30 +0900

    baekjoon 11053

---
+++ baekjoon/baekjoon/src/baekjoon_package/Test11053.java
@@ -0,0 +1,38 @@
+package baekjoon_package;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Test11053 {
+
+    private static int[] arr;
+    private static Integer[] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[N];
+        dp = new Integer[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            dp[i] = 1;
+
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = Arrays.stream(dp).mapToInt(x -> x).max().getAsInt();
+        System.out.println(max);
+    }
+}
