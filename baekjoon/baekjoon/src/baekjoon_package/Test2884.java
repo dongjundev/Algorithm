079b0bb17219138dd2a82bddddf6fcd94ad80238
dongjun <yh02195@gmail.com>
2022-11-09 18:16:09 +0900

    baekjoon 2884

---
+++ baekjoon/baekjoon/src/baekjoon_package/Test2884.java
@@ -0,0 +1,26 @@
+package baekjoon_package;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Test2884 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if (m - 45 < 0) {
+            if (h == 0) {
+                h = 24 - 1;
+            } else h-=1;
+            m = 60 + (m - 45);
+        } else {
+            m -= 45;
+        }
+
+        System.out.println(h + " " + m);
+    }
+}
